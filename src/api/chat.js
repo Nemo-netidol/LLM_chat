ca569abd25e5edf3756a5ef69b512c9e03b094dc
Nakorn Boonprasong <curseboyz@gmail.com>
2025-06-03 00:19:09 +0700

    Save current changes before initializing git flow

--- src/api/chat.js
+++ src/api/chat.js
@@ -46,6 +46,90 @@ app.post("/chat", async (req, res) => {
   }
 });
 
+app.post("/qwen", async (req, res) => {
+  try {
+
+    // console.log(req.body)
+    const messages = req.body.messages;
+    const role = req.body.messages[0].role; 
+    console.log(messages)
+    // console.log(role)  
+
+
+    const response = await fetch("http://localhost:1234/v1/chat/completions", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          model: "bartowski/Qwen2.5-32B-Instruct-GGUF",
+          messages: messages
+        })
+      });
+
+    const data = await response.json();
+    console.log("qwen response", data);
+    const raw = data.choices[0].message.content;
+    
+    const jsonString = raw.replace(/```json\n?/, '').replace(/```$/, '').trim();
+    const parsed = JSON.parse(jsonString);
+    console.log("parsed", parsed);
+    res.json({
+       "content": parsed.response,
+       "emotion": parsed.emotion
+      });
+
+  } catch (error) {
+    console.log("Express cannot send message")
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  } 
+});
+
+app.post("/prompt", async (req, res) => {
+  try {
+    const prompt_msg = req.body.messages[0].content;
+    const role = req.body.messages[0].role; 
+    // console.log(prompt_msg)
+    // console.log(role)  
+    // console.log(req.body.messages[0].content) 
+    // console.log(Object.keys(req)) 
+
+    const response = await fetch("http://localhost:1234/v1/chat/completions", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          model: "bartowski/Qwen2.5-32B-Instruct-GGUF",
+          messages: [
+            {
+                role: "system",
+                content: prompt_msg,
+            },
+          ],
+        }),
+      });
+
+    const data = await response.json();
+    // console.log(data.choices[0].message.content)
+    const systemPromptResponse = data.choices[0].message.content;
+
+    const jsonString = systemPromptResponse.replace(/```json\n?/, '').replace(/```$/, '').trim();
+    const parsed = JSON.parse(jsonString);
+
+    // console.log("systemPromptResponse:", parsed); 
+    // console.log("Express content:", parsed.response)
+    // console.log("Express emotion:", parsed.emotion)
+
+    res.json({
+       "content": parsed.response,
+       "emotion": parsed.emotion
+      }); 
+
+  } catch (error) {
+    console.log("Express cannot send message")
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  } 
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { config } from 'dotenv';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { config } from "dotenv";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
+    pproxy: {
+      port: 5173,
       '/api': {
         target: 'http://localhost:1234',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-  
-})
+});
